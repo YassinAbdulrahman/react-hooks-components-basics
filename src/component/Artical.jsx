fd0b3843c6f5dedb860cbb4bdcfecefa570531b8
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-02-13 14:57:56 +0300

    Done Work On this task

---
+++ src/component/Artical.jsx
@@ -0,0 +1,7 @@
+function Article() {
+    return (
+        <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    );
+}
+
+export default Article()
---
+++ src/component/Commet.jsx
@@ -0,0 +1,5 @@
+function Comment() {
+    return <div>Naturally, I agree with this article.</div>;
+}
+
+export default Comment()
